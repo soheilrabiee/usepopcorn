b775e26f6f082c4a1b02311f0fdeca6139419c05
Soheil <iamsoheil.r@gmail.com>
2025-09-01 21:16:40 +0330

    Add type checking for the props using PropTypes

--- src/StarRating.js
+++ src/StarRating.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import PropTypes from "prop-types";
 
 const containerStyle = {
     display: "flex",
@@ -8,6 +9,17 @@ const containerStyle = {
 
 const starContainerStyle = {
     display: "flex",
+};
+
+// (Type Checking) the property name should always be lowercase and the object inside should always be in uppercase
+StarRating.propTypes = {
+    maxRating: PropTypes.number,
+    defaultRating: PropTypes.number,
+    color: PropTypes.string,
+    size: PropTypes.number,
+    messages: PropTypes.array,
+    className: PropTypes.string,
+    onSetRating: PropTypes.func,
 };
 
 export default function StarRating({
